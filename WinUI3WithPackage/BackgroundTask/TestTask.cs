86e792559138587d5f97c7c6e04427e543faaefb
Mvk3aJKQh <KeithRoyMail@yahoo.com>
2025-02-20 17:22:21 -0800

    Add files via upload

---
+++ WinUI3WithPackage/BackgroundTask/TestTask.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+using Windows.ApplicationModel.Background;
+
+namespace BackgroundTask
+{
+	public sealed class TestTask : IBackgroundTask
+	{
+		public void Run(IBackgroundTaskInstance taskInstance)
+		{
+			Debug.WriteLine("Background " + taskInstance.Task.Name + " Starting...");
+		}
+	}
+}
